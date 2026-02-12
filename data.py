64d4d0bd332868a8ffff9760fa1fc5eecf28d49b
Lauro Pardinho <diaslauro7@gmail.com>
2026-02-12 16:26:19 -0300

    a

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-7bfc4572-e0ea-42e5-9fdb-fdf0d02d5ed3.containerhub.tripleten-services.com?lng=pt'
 ADDRESS_FROM = 	'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = 	'+1 123 123 12 12'
